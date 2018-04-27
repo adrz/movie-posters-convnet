bf59e74d02eeeba6a16b626d3cb231da8aab77cc
adriennouvellet <adrien.nouvellet@it4pme.fr>
2018-04-27 16:21:37 +0200

    dfb

--- static/js/plot3.js
+++ static/js/plot3.js
@@ -39,7 +39,8 @@ function plot(json, size){
     
     var zoom = d3.zoom().scaleExtent([0.2, 10])
 	.translateExtent([[-100, -100], [2000, 2000]]);
-    svg.call(zoom);
+    svg.call(zoom)
+	.call(zoom.transform, d3.zoomIdentity.scale(.2,.2));
     zoom.on("zoom", function(){
 	var transform = d3.event.transform;
 	var w = size[0] / Math.sqrt(transform.k);
@@ -51,7 +52,6 @@ function plot(json, size){
 	    .attr('width', w)
 	    .attr('height',h);
     });
-    zoom.translate([50,50]).scale(.2);
 };
 
 function click_img(num) {
